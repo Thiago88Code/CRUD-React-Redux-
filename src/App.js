ed6141429b42b2c1a8bd2f538ed58654edaf0195
ThiagoSz <thdev88@gmail.com>
2023-05-17 12:32:28 +0100

    refactoring code

--- src/App.js
+++ src/App.js
@@ -1,53 +1,49 @@
 import "./App.css"
-import {useState} from "react"
-import {useDispatch, useSelector} from "react-redux"
+import { useState } from "react"
+import { useDispatch, useSelector } from "react-redux"
 import { addUser, deleteUser, updateUsername } from "./features/UsersReducers"
-
-
-
 
 function App() {
 
   const dispatch = useDispatch()
-  const userList = useSelector((state)=>state.users.value)
+  const userList = useSelector((state) => state.users.value)
 
-  const[name, setName] = useState("");
-  const[username, setUsername] = useState("");
-  const[newUsername, setNewUsername] = useState("")
-  
+  const [name, setName] = useState("");
+  const [username, setUsername] = useState("");
+  const [newUsername, setNewUsername] = useState("")
 
 
   return (
     <>
-     <div className="container1">
-          <input type={"text"} value={name} onChange={(e)=>{setName(e.target.value)}} placeholder="name..."></input>
-          <input type={"text"} value={username} onChange={(e)=>{setUsername(e.target.value)}} placeholder="username..."></input>
-          <button onClick={()=>{
-              dispatch(addUser({id : userList[userList.length - 1].id + 1, name, username}))
-              setName("")
-              setUsername("")
-              }}>add User
-          </button>
+      <div className="container1">
+        <input type={"text"} value={name} onChange={(e) => { setName(e.target.value) }} placeholder="name..."></input>
+        <input type={"text"} value={username} onChange={(e) => { setUsername(e.target.value) }} placeholder="username..."></input>
+        <button onClick={() => {
+          dispatch(addUser({ id: userList[userList.length - 1].id + 1, name, username }))
+          setName("")
+          setUsername("")
+        }}>add User
+        </button>
       </div>
 
-       <div  className="container2">
-         {userList.map((user)=>{
-           return <div key={user.id}>
-                    <h1>{user.name}</h1>
-                    <h1>{user.username}</h1>
-                    
-                    <input type={"text"} value ={newUsername} onChange={(e)=>{setNewUsername(e.target.value)}} placeholder="username..."></input>
-                    
-                      <button onClick={()=>{
-                          dispatch(updateUsername({id : user.id, username: newUsername}))
-                          setNewUsername("")
-                          }}>Update Username
-                      </button>
-                      <button onClick={()=>{dispatch(deleteUser({id : user.id}))}}>Delete</button>
-                  </div>
-                  })}
-       </div>
-     </>
+      <div className="container2">
+        {userList.map((user) => {
+          return <div key={user.id}>
+            <h1>{user.name}</h1>
+            <h1>{user.username}</h1>
+
+            <input type={"text"} value={newUsername} onChange={(e) => { setNewUsername(e.target.value) }} placeholder="username..."></input>
+
+            <button onClick={() => {
+              dispatch(updateUsername({ id: user.id, username: newUsername }))
+              setNewUsername("")
+            }}>Update Username
+            </button>
+            <button onClick={() => { dispatch(deleteUser({ id: user.id })) }}>Delete</button>
+          </div>
+        })}
+      </div>
+    </>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import "./index.css"
-import {configureStore} from "@reduxjs/toolkit"
-import {Provider} from "react-redux"
-import UsersReducer from"./features/UsersReducers"
+import { configureStore } from "@reduxjs/toolkit"
+import { Provider } from "react-redux"
+import UsersReducer from "./features/UsersReducers"
 
 const store = configureStore({
   reducer: {
